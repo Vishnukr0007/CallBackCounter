ef4c89ee687230e4e0643a64216b48c7538ecf8c
Vishnukr0007 <vishnukrishnankutty54@gmail.com>
2024-09-04 18:25:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import Button from './Button';
+import Counter from './Counter';
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const handleIncrement = useCallback(() => {
+    setCount1((prevCount1) => prevCount1 + 1);
+  }, []); 
+
+  const handleDecrement = useCallback(() => {
+    setCount2((prevCount2) => prevCount2 - 1);
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='sh'>
+      <Counter text="counter1" count={count1} />
+      <Button handle={handleIncrement}>increment</Button>
+      <Counter text="counter2" count={count2} />
+      <Button handle={handleDecrement}>decrement</Button>
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Counter({ text, count }) {
+  console.log('rendering');
+
+  return (
+    <div>
+      {text} : {count}
+    </div>
+  );
+}
+
+export default React.memo(Counter);
